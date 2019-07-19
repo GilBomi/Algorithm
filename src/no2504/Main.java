99b746c7bc61ceb063db7b50a9098fa5ca1f0288
gilbomi <gil1122@naver.com>
2019-07-20 01:24:35 +0900

    백준

---
+++ src/no2504/Main.java
@@ -0,0 +1,40 @@
+package no2504;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		Stack<String> stack=new Stack<>();
+		String s=input.nextLine();
+		String pre=s.substring(0,1);
+		boolean result=true;
+		for(int i=1;i<s.length();i++) {
+			switch(s.substring(i, i+1)) {
+			case "(":
+			case "[":
+				stack.push(s.substring(i,i+1));
+				break;
+			case ")":
+				if(stack.size()==0) 
+					result=false;
+				else if(!stack.pop().equals("("))
+					result=false;
+				break;
+			case "]":
+				if(stack.size()==0) 
+					result=false;
+				else if(!stack.pop().equals("["))
+					result=false;
+				break;
+			}
+			if(!result)
+				break;
+		}
+		if(result && stack.size()==0) 
+			System.out.println();
+		else
+			System.out.println("0");
+	}
+}
---
+++ src/no4949/Main.java
@@ -0,0 +1,45 @@
+package no4949;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		String s="";
+		while(true) {
+			Stack<String> stack=new Stack<>();
+			s=input.nextLine();
+			if(s.equals("."))
+				break;
+			boolean result=true;
+			for(int i=0;i<s.length();i++) {
+				switch(s.substring(i, i+1)) {
+				case "(":
+				case "[":
+					stack.push(s.substring(i,i+1));
+					break;
+				case ")":
+					if(stack.size()==0) 
+						result=false;
+					else if(!stack.pop().equals("("))
+						result=false;
+					break;
+				case "]":
+					if(stack.size()==0) 
+						result=false;
+					else if(!stack.pop().equals("["))
+						result=false;
+					break;
+				}
+				if(!result) 
+					break;
+			}
+			if(result && stack.size()==0 )
+				System.out.println("yes");
+			else 
+				System.out.println("no");
+		}
+	
+	}		
+}
