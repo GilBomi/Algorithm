5b9497f33bf60801f5dc0a744614bcddf21f343a
gilbomi <gil1122@naver.com>
2019-09-21 21:25:15 +0900

    1325 자바11에서는 맞았다고 나오는데 그냥 자바에서는 시간초과나옴

--- src/no1325/Main.java
+++ src/no1325/Main.java
@@ -11,32 +11,26 @@ public class Main {
 	static List<Integer>[] list;
 	static boolean[] visited;
 	static int[] depth;
-		
-	public static int dfs(int start) {
-		int max=0;
-		for(int i:list[start]) {
-			if(visited[i])
-				continue;
-			visited[i]=true;
-			int depth=dfs(i);
-			if(depth>max) 
-				max=depth;
+
+	public static void dfs(int start) {
+		//		visited[start]=true;
+		//		for(int i:list[start]) {
+		//			if(visited[i])
+		//				continue;
+		//			System.out.println("깊이:"+i);
+		//			dfs(i);
+		//		}
+
+		visited[start] = true;
+		//System.out.println("깊이:"+(start+1));
+		for (int i :list[start]) {
+			if (!visited[i]) {
+				depth[i]++;
+				dfs(i);
+			}
 		}
-		return max+1;
-	}
-	public static int findMax() {
-		int max=0;
-		for(int i=0;i<list.length;i++) {
-			if(max<depth[i])
-				max=depth[i];
-		}
-		return max;
-	}
-	public static void 같은값찾기(int max) {
-		for(int i=0;i<list.length;i++) {
-			if(depth[i]==max)
-				System.out.print(i+1+" ");
-		}
+
+
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
@@ -46,19 +40,27 @@ public class Main {
 		list=new ArrayList[n];
 		for(int i=0;i<n;i++)
 			list[i]=new ArrayList<>();
-		
+
 		for(int i=0;i<m;i++) {
 			token=new StringTokenizer(reader.readLine());
 			int m1=Integer.parseInt(token.nextToken())-1;
 			int m2=Integer.parseInt(token.nextToken())-1;
-			list[m2].add(m1);
+			list[m1].add(m2);
 		}
 		depth=new int[n];
-		for(int i=0;i<n;i++) { // depth배열에 해당 인덱스마다 최대 깊이(dfs)값 저장
+		for(int i=0;i<n;i++) {
 			visited=new boolean[n];
-			depth[i]=dfs(i);
+			dfs(i);
 		}
-		같은값찾기(findMax());
+		int max=0;
+		for(int i=0;i<n;i++) {
+			if(max<depth[i])
+				max=depth[i];
+		}
+		for(int i=0;i<n;i++) {
+			if(max==depth[i])
+				System.out.print(i+1+" ");
+		}
 		
 	}
 }
