68f96633cca6e4d40edcbfa1f2b5d912a349cd39
bom04 <gmaru6420@gmail.com>
2022-01-12 23:55:49 +0900

    백준

---
+++ src/no1439/Main.java
@@ -0,0 +1,32 @@
+package no1439;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+		char[] a=reader.readLine().toCharArray();
+		
+		int num_zero=0,num_one=0;
+		char prev=a[0];
+		if(prev=='0')
+			num_one++;
+		else
+			num_zero++;
+		for(int i=0;i<a.length;i++) {
+			if(prev!=a[i]) {
+				if(prev=='0') {
+					num_zero++;
+				} else {
+					num_one++;
+				}
+				prev=a[i];
+			}
+		}
+		System.out.println(num_zero+" "+num_one);
+		System.out.println(Math.min(num_zero, num_one));
+	}
+}
